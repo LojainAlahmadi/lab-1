3bd69f6071170a78566379cd3446fb2cd327bff4
Lojain Alahmadi <leidalahmadi@stu.kau.edu.sa>
2023-03-18 20:04:21 +0300

    First commit.

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author lojai
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hello,I am Lojain i hope you have a great day :)");
+
+    int x = 5;
+    int z = 1;
+    int sum = x+z;
+    System.out.print(sum);
+    }
+    
+}
